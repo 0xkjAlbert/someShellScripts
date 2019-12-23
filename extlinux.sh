dcfb6a5ba440c6a6e2c51f189aad8911702e798f
0xkjAlbert <root@centos7.LinuxSpace>
2019-12-23 20:22:07 +0800

    extlinux

--- extlinux.sh
+++ extlinux.sh
@@ -2,12 +2,23 @@
 #
 #********************************************************************
 #Author:		kjAlbert
-#Date:			2019-12-22
-#FileName： 		firewalld.sh
+#Date:			2019-12-23
+#FileName： 		extlinux.sh
 #Description:		The test script
 #Copyright (C): 	2019 All rights reserved
 #********************************************************************
 #
-systemctl start firewalld
-firewall-cmd --add-service ssh
-firewall-cmd --add-service httpd
+
+# 判断是否是8-32G之间的磁盘，如果是就认为是U盘
+
+#分区
+fdisk /dev/sdb <<EOF
+n
+p
+
+
+
+w
+
+EOF
+
